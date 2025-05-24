a77110e7c84731661968cd302d0124ce86853584
lagman5006 <asliddin5006@gmail.com>
2025-05-24 17:50:39 +0500

    uy ishi test fileni ichida

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_ui_chizish/checkout.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:responsive_ui_chizish/widgets/test.dart';
 
 void main() {
@@ -11,6 +11,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: TestTasks());
+    return ScreenUtilInit(
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()),
+    );
   }
 }
--- lib/widgets/test.dart
+++ lib/widgets/test.dart
@@ -1,25 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class TestTasks extends StatelessWidget {
-  const TestTasks({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.white,
+      body: Container(
+        color: Color(0xFF7B61FF),
+        child: Stack(
+          alignment: Alignment.center,
           children: [
-            RichText(text: TextSpan(
-              text: "Flutter ",style: TextStyle(
-              color: Colors.blue
-            ),children: [
-              TextSpan(text: "Widgets ",style: TextStyle(fontStyle: FontStyle.italic,color: Colors.green)),
-              TextSpan(text: "Tutorial ",style: TextStyle(decoration: TextDecoration.underline,color: Colors.red)),
-
-            ]
-            ))
+            Positioned(
+              bottom: 330,
+              child: Image.asset(
+                "assets/images/person.webp",
+                width: 350.w,
+                height: 350.h,
+              ),
+            ),
+            Positioned(
+              bottom: 60,
+              child: Container(
+                width: 300.w,
+                height: 250.h,
+                decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(20)),
+                child: Column(
+                  spacing: 20,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("  Discover your next skill\n"
+                            "Learn anything you want!",style: TextStyle(fontSize: 25,color: Color(0xFF493AA0,),fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                      Text("Discover the things you want to \n"
+                          "    learn and grow with them",style: TextStyle(color: Colors.black.withOpacity(0.3)),)
+                    ],),
+                    SizedBox(height: 17.h,),
+                    Container(
+                      alignment: Alignment.center,
+                      width: 140,
+                      height: 50,
+                      decoration: BoxDecoration(color: Color(0xFF493AA0),borderRadius: BorderRadius.circular(15)),
+                      child: Text("Get Started",style: TextStyle(color: Colors.white),) ,
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
